22d88df123e430c066e3bd57eeb115511f001920
Brendon Hablutzel <booklvrbren@gmail.com>
2026-02-13 11:46:07 -0500

    add github receive endpoint

--- webhook-server/src/main.rs
+++ webhook-server/src/main.rs
@@ -1,9 +1,12 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get};
+use serde_json::Value;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/github", axum::routing::post(webhook));
 
     let listener = TcpListener::bind("0.0.0.0:8001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -11,4 +14,9 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn webhook(Json(payload): Json<Value>) -> Json<Value> {
+    println!("{}", serde_json::to_string_pretty(&payload).unwrap());
+    Json(payload)
 }
