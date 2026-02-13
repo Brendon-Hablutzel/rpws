736be02fc9b4920c444beaca595996ba175fc30a
Brendon Hablutzel <booklvrbren@gmail.com>
2026-02-13 11:20:27 -0500

    add webhook server basic

---
+++ webhook-server/src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = TcpListener::bind("0.0.0.0:8001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
